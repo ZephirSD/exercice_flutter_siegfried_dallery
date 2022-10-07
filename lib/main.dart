ff717f085a383b79e598ebedb710db620a8e3029
ZephirSD <87165268+ZephirSD@users.noreply.github.com>
2022-10-07 13:04:39 +0400

    Ajout de la fonctionnalité pour faire des opérations

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'Composant/buttonCalc.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,109 +9,175 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MyHomePage extends StatelessWidget {
+  MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HomeCalc(),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class HomeCalc extends StatefulWidget {
+  HomeCalc({Key? key}) : super(key: key);
+  @override
+  State<HomeCalc> createState() => _HomeCalcState();
+}
 
-  void _incrementCounter() {
+class _HomeCalcState extends State<HomeCalc> {
+  int premierNb = 0;
+  int secondNb = 0;
+  int total = 0;
+  String operation = "";
+  String textCalc = "";
+  clickNombre(textValue) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      textCalc = textValue;
+      if (operation != "") {
+        secondNb = int.parse(textCalc);
+        // print('second${secondNb}');
+      } else {
+        premierNb = int.parse(textCalc);
+        // print('premier${premierNb}');
+      }
+    });
+  }
+
+  clickOperator(textValue) {
+    setState(() {
+      if (textValue != "=") {
+        operation = textValue;
+      }
+      if (textValue == "=" && operation == "+") {
+        total = (premierNb + secondNb);
+      } else if (textValue == "=" && operation == "-") {
+        total = (premierNb - secondNb);
+      } else if (textValue == "=" && operation == "*") {
+        total = (premierNb * secondNb);
+      } else if (textValue == "=" && operation == "/") {
+        total = (premierNb / secondNb) as int;
+      }
+      if (total > 0) {
+        textCalc = total.toString();
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Container(
+      child: Column(
+        verticalDirection: VerticalDirection.up,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              ButtonCalc(
+                "0",
+                funcCal: () => clickNombre("0"),
+                flex: 2,
+              ),
+              ButtonCalc(
+                ",",
+                funcCal: () => clickNombre(","),
+              ),
+              ButtonCalc(
+                "=",
+                funcCal: () => clickOperator("="),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              ButtonCalc(
+                "1",
+                funcCal: () => clickNombre("1"),
+              ),
+              ButtonCalc(
+                "2",
+                funcCal: () => clickNombre("2"),
+              ),
+              ButtonCalc(
+                "3",
+                funcCal: () => clickNombre("3"),
+              ),
+              ButtonCalc(
+                "+",
+                funcCal: () => clickOperator("+"),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              ButtonCalc(
+                "4",
+                funcCal: () => clickNombre("4"),
+              ),
+              ButtonCalc(
+                "5",
+                funcCal: () => clickNombre("5"),
+              ),
+              ButtonCalc(
+                "6",
+                funcCal: () => clickNombre("6"),
+              ),
+              ButtonCalc(
+                "-",
+                funcCal: () => clickOperator("-"),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              ButtonCalc(
+                "7",
+                funcCal: () => clickNombre("7"),
+              ),
+              ButtonCalc(
+                "8",
+                funcCal: () => clickNombre("8"),
+              ),
+              ButtonCalc(
+                "9",
+                funcCal: () => clickNombre("9"),
+              ),
+              ButtonCalc(
+                "*",
+                funcCal: () => clickOperator("*"),
+              ),
+            ],
+          ),
+          Container(
+            child: Text(
+              textCalc,
+              style: TextStyle(fontSize: 28),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+            alignment: Alignment(1.0, 1.0),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
